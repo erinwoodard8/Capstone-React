7aeb214626fe74a1dbfc4b65f70f75fbbbb0a033
Deepshikha Sharma <deepshikha.guleria@gmail.com>
2021-07-07 16:53:51 -0400

    created login.js and Signup.js
    Signed-off-by: Deepshikha Sharma <deepshikha.guleria@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import React from 'react';
 import {Switch, Route } from "react-router-dom";
 import Login from '../src/Homepage/Login.js'
+import Signup from '../src/Homepage/Signup'
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
       </Switch>
     </div>
   );
--- src/Homepage/Signup.js
+++ src/Homepage/Signup.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useState } from "react";
+
 
 const Signup = () => {
+    const [state, setState] = useState(false);
+
+    const render = () => {
+        if (state) {
+          return <Signup>Register</Signup>;
+        } else {
+          return <h1>This is signup page</h1>
+        }
+      };
 
     //will have option for user to signup for google
     //or allow user to enter a email, username, password
@@ -10,7 +20,20 @@ const Signup = () => {
     //will post the new user to the backend DB
 
     return(
-        <div><h1>This is the Signup component</h1></div>
+        <div>
+        <input type="name" className="userName" placeholder="enter name" />
+        
+         <br></br>
+        <input type="email" className="email" placeholder="example@example.com" />
+        
+         <div className="signup">
+         <br></br>
+        <input type="password" className="password" placeholder="Enter password" />
+        </div>
+        <br></br>
+        <button onClick={() => setState(true)}>LogIn</button>
+        {render()}
+        </div>
     )
 };
 
