d5b54e269773b5fc35e7c845b0b6e9a6f4905a51
erinwoodard8 <erinwoodard8@gmail.com>
2021-07-07 13:05:09 -0400

    showing off funct components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import React from 'react';
+import {Switch, Route } from "react-router-dom";
+import Login from '../src/Homepage/Login.js'
 
 function App() {
   return (
     <div className="App">
-        <h3>Hello world Franky</h3>
-        <h3>Hello world Deepshikha</h3>
-        <h1>1st day of movie app</h1>
-        <h2>Jonathan was here</h2>
-        <h2>Anu was here</h2>
+      <Switch>
+        <Route path="/login" component={Login} />
+      </Switch>
     </div>
   );
 }
--- src/Homepage/Login.js
+++ src/Homepage/Login.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import Signup from './Signup'
 
 const Login = () => {
+  const [state, setState] = useState(false);
 
-    //have login fields email and password
-    //allow to login with google
-    //once login/authorized, page will render movie search
-    //have a link to allow user to signup
+  //have login fields email and password
+  //allow to login with google
+  //once login/authorized, page will render movie search
+  //have a link to allow user to signup
 
-  return <div></div>;
+  const render = () => {
+    if (state) {
+      return <Signup></Signup>;
+    } else {
+      return <h1>Button was NOT clicked</h1>
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={() => setState(true)}>Click me</button>
+      {render()}
+    </div>
+  );
 };
+
+export default Login;
