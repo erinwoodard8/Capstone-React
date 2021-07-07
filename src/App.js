04bb3ac2c81bf57d045d5eea880650c97fab5e3c
erinwoodard8 <erinwoodard8@gmail.com>
2021-07-07 08:53:15 -0400

    cleaned up App.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <h3>Hello world Franky</h3>
         <h3>Hello world Deepshikha</h3>
         <h1>1st day of movie app</h1>
         <h2>Jonathan was here</h2>
         <h2>Anu was here</h2>
-      </header>
     </div>
   );
 }
