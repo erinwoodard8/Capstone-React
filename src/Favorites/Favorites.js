23387cc7442367aad2842856e68e3a9d144ffc91
erinwoodard8 <erinwoodard8@gmail.com>
2021-07-07 10:46:54 -0400

    added basic components

---
+++ src/Favorites/Favorites.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Favorites = () => {
+
+    //get request to retrieve current login user favorite movies
+    //list the movies here in cards
+
+    return(<div></div>)
+}
---
+++ src/Homepage/Signup.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Signup = () => {
+
+    //will have option for user to signup for google
+    //or allow user to enter a email, username, password
+    //if user uses google, we still need them to be able to 
+    //enter a username
+    //there will be function that handles a post request that
+    //will post the new user to the backend DB
+
+    return(
+        <div></div>
+    )
+}
