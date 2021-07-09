2546139296c065e7463d90a6afb70a23311e7002
erinwoodard8 <erinwoodard8@gmail.com>
2021-07-09 17:02:34 -0400

    made testing component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Switch, Route } from "react-router-dom";
 import Login from '../src/Homepage/Login.js';
 import Signup from '../src/Homepage/Signup';
 import NavbarI from './Homepage/Navbar';
+import Test from './test';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route path="/navbar" component={NavbarI} />
+        <Route path="/test" component={Test} />
       </Switch>
     </div>
   );
