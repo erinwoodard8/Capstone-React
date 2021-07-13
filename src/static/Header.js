e2d7fa303673e3fbeba92f4ee7db6880afbd6ce2
erinwoodard8 <erinwoodard8@gmail.com>
2021-07-13 17:01:04 -0400

    navbar edits

--- src/static/Header.js
+++ src/static/Header.js
@@ -1,18 +1,34 @@
-import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
-import React from "react";
+import { Navbar, Nav } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
 import MyflixLogo4 from "./MyflixLogo4.png";
 import "../styles/header.css"
 
 
-
-
-
-
-
 const Header = () => {
 
+  const [userState, setUserState] = useState({});
 
+  useEffect(() => {
+    postUser();
+  }, []);
   
+  const postUser = () => {
+    let data = userState;
+    fetch("http://localhost:8080/users/post/google", {
+      method: "POST",
+      credentials: "include",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => setUserState(json))
+      .then((json) => console.log(json));
+  };
+
+
+
   return (
    
 <div>
@@ -28,10 +44,13 @@ const Header = () => {
   <Navbar.Toggle className="hamburger" aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="/">Home</Nav.Link>
+      <Nav.Link href="/">Search</Nav.Link>
       <Nav.Link href="/favorites">Favorites</Nav.Link>
+      <Nav.Link className="user">Logged in as: {userState.username}</Nav.Link>
       <Nav.Link href="http://localhost:8080/logout">Logout</Nav.Link>
+      
     </Nav>
+    
   </Navbar.Collapse>
 </Navbar>
 </div>
