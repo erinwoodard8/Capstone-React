e6bedf260f2f237f87e3ae0244ee774a714c19d1
aprila13 <aprilacraig1@gmail.com>
2021-07-14 10:46:39 -0400

    Added video

--- src/Homepage/Login.js
+++ src/Homepage/Login.js
@@ -2,14 +2,18 @@ import React from "react";
 // import "../styles/common.css";
 import MyflixLogo4 from "../static/MyflixLogo4.png";
 import "../styles/login.css";
+import videos from "../styles/videos.mp4";
+
 
 const Login = () => {
   return (    
     <div id="loginform">
+      <video autoPlay loop muted={true} height="150px" width="300px">
+        <source src={videos} type="video/mp4"/>
+      </video>
       <div className="image">
         <img src={MyflixLogo4} alt="Logo" />
       </div>
-
       <a
         className="button-link"
         href="http://localhost:8080/oauth2/authorization/google"
