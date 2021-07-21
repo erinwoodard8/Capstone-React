2cffaf6302a80f68398bcfaf42607af3ec1cc3e1
erinwoodard8 <erinwoodard8@gmail.com>
2021-07-21 14:01:58 -0400

    fixed Favorites.js

--- src/Favorites/Favorites.js
+++ src/Favorites/Favorites.js
@@ -3,11 +3,11 @@ import Header from "../static/Header";
 import MovieCard from "../static/MovieCard";
 import "../styles/favorites.css";
 import { Row, Col, Container } from "react-bootstrap";
-import { styles } from "ansi-colors";
-import noMovieTrans from "../styles/noMovieTrans.png";
+import noMovieTrans from "../static/noMovieTrans.png";
 
 const Favorites = () => {
   const [listState, setListState] = useState([]);
+  const [renderState, setRenderState] = useState(true);
 
   useEffect(() => {
     getFavorites();
@@ -23,7 +23,7 @@ const Favorites = () => {
     let movieList = [];
     console.log(favorites);
 
-    if(favorites == null) {
+    if (favorites == null) {
       setListState(movieList);
     } else {
       for (let i = 0; i < favorites.length; i++) {
@@ -39,23 +39,65 @@ const Favorites = () => {
       setListState(movieList);
     }
   }
-  console.log(listState);
+  async function removeFavorite(movie) {
+    const userResponse = await fetch("http://localhost:8080/users/login", {
+      method: "GET",
+      credentials: "include",
+    });
+    const user = await userResponse.json();
+    const favorites = user.favoriteMovies;
+    const movieIndex = favorites.indexOf(movie.id);
+    favorites.splice(movieIndex, 1);
+    
+    const response = await fetch("http://localhost:8080/users/favorites", {
+      method: "POST",
+      credentials: "include",
+      body: JSON.stringify(user),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    setRenderState(!renderState);
+  }
 
   return (
     <div>
-    <Header />
-  
-    <div className="d-flex justify-content-center noFavLogo">
-    {listState.length == 0 ? <div className="noFavorites" >
-      <img src={noMovieTrans} />
-      </div> 
-      : <Container flex={true}>
-    </Container>}  
+      <Header />
+      {listState.length == 0 ? (
+        <div className="noFavorites">
+          <img className="img-fluid" width="50%"  src={noMovieTrans} />
+        </div>
+      ) : (
+        <Container flex={true}>
+          {listState.length % 3 == 0 ? (
+            <Row className="row justify-content-md-center">
+              {listState.map((movie) => (
+                <Col key={movie.id} className="justify-content-md-center">
+                  <MovieCard
+                    movie={movie}
+                    listState={listState}
+                    removeFavorite={removeFavorite}
+                  />
+                </Col>
+              ))}
+            </Row>
+          ) : (
+            <Row className="row justify-content-md-center">
+              {listState.map((movie) => (
+                <Col key={movie.id} className="justify-content-md-center">
+                  <MovieCard
+                    movie={movie}
+                    listState={listState}
+                    removeFavorite={removeFavorite}
+                  />
+                </Col>
+              ))}
+              <Col></Col>
+            </Row>
+          )}
+        </Container>
+      )}
     </div>
-    <body>
-<div ></div>
-    </body>
-  </div>
   );
 };
 export default Favorites;
