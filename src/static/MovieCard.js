dcc6a9e71919b11cc1409208c5012d711093bfef
erinwoodard8 <erinwoodard8@gmail.com>
2021-07-21 13:05:42 -0400

    added getMovieInfo funct and button on card

--- src/static/MovieCard.js
+++ src/static/MovieCard.js
@@ -4,7 +4,7 @@ import Button from "react-bootstrap/Button";
 import "../styles/movieCard.css";
 
 
-const MovieCard = ({ movie, listState, removeFavorite }) => {
+const MovieCard = ({ movie, listState, removeFavorite, getMovieInfo }) => {
   const buttonRender = () => {
     if (listState == undefined) {
       return <p></p>;
@@ -25,12 +25,15 @@ const MovieCard = ({ movie, listState, removeFavorite }) => {
       return <p></p>;
     }
   };
+
+//Send movie.id to the results page as a prop
   return (
     <Card className="card-container" style={{ width: "18rem" }}>
       <Card.Img className="card-image" variant="top" src={movie.image} />
       <Card.Body className="card-body">
         <Card.Title>{movie.title}</Card.Title>
         {buttonRender()}
+        <Button onClick={() => getMovieInfo(movie)}>More Info</Button>
       </Card.Body>
     </Card>
   );
